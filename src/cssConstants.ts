6ff6dbb54ce3c683340af378d7a04af62771f44e
Jean Gregolon <jean.gregolon@gmail.com>
2023-05-19 12:42:27 -0300

    CSS constants

---
+++ src/cssConstants.ts
@@ -0,0 +1,8 @@
+export const black = "#000000";
+export const white = "#ffffff";
+export const darkBlue = "#023047";
+export const mediumBlue = "#219ebc";
+export const lightBlue = "#8ecae6";
+export const orange = "#fb8500";
+export const yellow = "#ffb703";
+export const gray = "#ccc";
