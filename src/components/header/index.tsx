e1e43cadde42a70a92cf594545b1e888b69101c2
Jean Gregolon <jean.gregolon@gmail.com>
2023-05-19 12:40:13 -0300

    Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,37 @@
+import { Container, Logo, Menu, MenuItem } from "./styles";
+
+export const Header = () => {
+  const handleMenuItemClick = (elementId: string) => {
+    const elementDiv = document.getElementById(elementId);
+    if (elementDiv) {
+      window.scrollTo({
+        top: elementDiv.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <header>
+      <Container>
+        <a href="/">
+          <Logo src="/assets/logo-h.png" alt="Logo" />
+        </a>
+        <Menu>
+          <MenuItem onClick={() => handleMenuItemClick("tecnologias")}>
+            Tecnologias
+          </MenuItem>
+          <MenuItem onClick={() => handleMenuItemClick("serviços")}>
+            Serviços
+          </MenuItem>
+          {/* <MenuItem onClick={() => handleMenuItemClick("projetos")}>
+            Projetos
+          </MenuItem> */}
+          <MenuItem onClick={() => handleMenuItemClick("contato")}>
+            Contato
+          </MenuItem>
+        </Menu>
+      </Container>
+    </header>
+  );
+};
---
+++ src/components/header/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { darkBlue, mediumBlue, white } from "../../cssConstants";
+
+export const Container = styled.div`
+  background-color: ${white};
+  height: 70px;
+  border-radius: 6px;
+  box-shadow: 0 1px 32px rgba(0, 0, 0, 0.15);
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1366px;
+  margin: 0 auto;
+`;
+
+export const Logo = styled.img`
+  height: 50px;
+`;
+
+export const Menu = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0;
+`;
+
+export const MenuItem = styled.li`
+  margin-left: 20px;
+  cursor: pointer;
+  color: ${darkBlue};
+
+  &:hover {
+    color: ${mediumBlue};
+  }
+`;
